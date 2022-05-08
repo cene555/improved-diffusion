6afb2961ddd8cc9fe4b3f0e20b0feb91ba495f57
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2022-05-08 12:23:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="improved-diffusion",
-    py_modules=["improved_diffusion"],
+    packages=["improved_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
